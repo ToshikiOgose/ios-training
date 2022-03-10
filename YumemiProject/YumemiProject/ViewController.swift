d42abbc5f2acbc8f08bb3a7230e66a5aca7f8dbd
ToshikiOgose <toshiki.ogose@kinto-mobility.biz>
2022-03-10 16:51:03 +0900

    プロジェクト作成と依存パッケージ取得

---
+++ YumemiProject/YumemiProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YumemiProject
+//
+//  Created by 生越 歳規（Toshiki Ogose） on 2022/03/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
