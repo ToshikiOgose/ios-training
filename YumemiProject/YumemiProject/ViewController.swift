e257adb3053ffa3e1e3812f2ed53047ade494283
ToshikiOgose <toshiki.ogose@kinto-mobility.biz>
2022-03-11 10:10:40 +0900

    ボタンクリック機能追加 ボタン押下時に天気情報取得APIを呼び、結果を受け取り、イメージ画像をUIImageViewに挿入後、画像の色付けを実施

--- YumemiProject/YumemiProject/ViewController.swift
+++ YumemiProject/YumemiProject/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
-
+import Foundation
+import YumemiWeather
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var reloadButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func fetchWeatherData(_ sender: Any) {
+        let fetchedWeatherData = YumemiWeather.fetchWeather()
+        let fetchedDataImage = UIImage(named:fetchedWeatherData)
+        //fetchedDataImage?.withTintColor(.orange)
+        weatherImageView.image = fetchedDataImage
+        switch fetchedWeatherData {
+        case "rainy":
+            weatherImageView.tintColor = .blue
+        case "sunny":
+            weatherImageView.tintColor = .red
+        default:
+            weatherImageView.tintColor = .gray
+        }
+    }
     
 }
 
